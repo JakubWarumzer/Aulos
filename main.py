9dbc4c35ea24b0116db73109bcbcf37d376cc415
Harvey Dent <jakubwarumzer@gmail.com>
2018-04-09 01:01:37 +0200

    Views and sorting by them included

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from collections import namedtuple
+from namedlist import namedlist
 
 #Simple comparing between two dates in following string format: 'yyyy-mm-dd'
 def is_older_date(first_date, second_date):
@@ -15,6 +16,23 @@ def is_date_in_range(date, dates_range):
     if is_older_date(dates_range.end, date): return False
 
     return True
+
+def get_videos_views(videos):
+    #Constructing API Call
+    ids = ''
+    for v in videos: ids = ids + v.id + ','
+    
+    api_call = f'https://www.googleapis.com/youtube/v3/videos?part=statistics&id={ids}&key={API_KEY}'
+
+    #Making a call & parsing output
+    r = requests.get(api_call)
+    call_output = r.json()
+
+    items = call_output['items']
+
+    for counter, item in enumerate(items):
+        videos[counter].views = item['statistics']['viewCount']
+    
 
 def gather_channel_videos(channel_id, page_token=''):
     #Preparing API Call URL
@@ -33,7 +51,7 @@ def gather_channel_videos(channel_id, page_token=''):
     for upload in items:
         snippet = upload['snippet']
         gathered_videos.append(Video(title=snippet['title'], id=snippet['resourceId']['videoId'],
-                                     date=snippet['publishedAt'].split('T', 1)[0]))
+                                     date=snippet['publishedAt'].split('T', 1)[0], views=-1))
 
     #Gathering further until the end
     try:
@@ -49,11 +67,8 @@ API_KEY = 'AIzaSyBI7omQ3Wja1wVMNfVwJbwUh4agH34D56Y'
 channel_id = 'UUJHA_jMfCvEnv-3kRjTCQXw' #Binging with Babish
 
 #Preparing containers
-Video = namedtuple('Video', 'title id date')
+Video = namedlist('Video', 'title id date views')
 videos = gather_channel_videos(channel_id)
-
-#Sorting aquired data by date
-sorted(videos, key=lambda x:x.date)
 
 #Setting up data range
 DateRange = namedtuple('DateRange', 'start end')
@@ -62,5 +77,6 @@ date_range = DateRange('2018-01-01', '2018-03-03')
 #Removing videos out of our date range
 videos = [v for v in videos if is_date_in_range(v.date, date_range)]
 
-for v in videos: 
-    print(f'{v.title}-{v.date}')
+#Finally, including views and sorting list by them
+get_videos_views(videos)
+videos.sort(key=lambda x:int(x.views))
